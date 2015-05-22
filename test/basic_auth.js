8339b4bf9d430885694afbf689400411c8cc11a1
Martin E. Zulliger <mzulliger@gmail.com>
2015-05-22 20:24:45 +0200

    And now more tests

---
+++ test/basic_auth.js
@@ -0,0 +1,76 @@
+/*jslint node: true nomen: true*/
+/*global describe, it, before, beforeEach*/
+'use strict';
+
+var rewire = require('rewire');
+var expect = require('chai').expect;
+var basic = rewire('../lib/auth/basic');
+
+describe('basic_auth', function () {
+    var params, auth_pap, auth_chap;
+    
+    before(function () {
+        auth_pap = basic.__get__('auth_pap');
+        auth_chap = basic.__get__('auth_chap');
+    });
+    
+    beforeEach(function () {
+        params = {
+            req: {
+                attributes: {
+                    'User-Name': 'User'
+                }
+            },
+            user: {
+                attributes: {
+                    'Cleartext-Password': 'clientPass'
+                }
+            },
+            res: {
+                code: 'Access-Reject'
+            }
+        };
+    });
+    
+    describe('pap', function () {
+        it('should authenticate with PAP', function (done) {
+            params.req.attributes['User-Password'] = 'clientPass';
+
+            auth_pap(params, null, function () {
+                expect(params.res.code).to.be.equal('Access-Accept');
+                done();
+            });
+        });
+    
+        it('should fail to authenticate with PAP', function (done) {
+            params.req.attributes['User-Password'] = 'wrongPass';
+            
+            auth_pap(params, null, function () {
+                expect(params.res.code).to.be.equal('Access-Reject');
+                done();
+            });
+        });
+    });
+
+    describe('chap', function() {
+        it('should authenticate with CHAP', function (done) {
+            params.req.attributes['CHAP-Challenge'] = new Buffer('91768eb5f10720bae1ddc3d6df3c1274', 'hex');
+            params.req.attributes['CHAP-Password'] = new Buffer('6100a0c14131b69a04ab344a21054483d1', 'hex');
+        
+            auth_chap(params, null, function () {
+                expect(params.res.code).to.be.equal('Access-Accept');
+                done();
+            });
+        });
+
+        it('should fail to authenticate with CHAP', function (done) {
+            params.req.attributes['CHAP-Challenge'] = new Buffer('91768eb5f10720bae1ddc3d6df3c1274', 'hex');
+            params.req.attributes['CHAP-Password'] = new Buffer('6100a0c14131b69a04ab344a21054483d2', 'hex');
+        
+            auth_chap(params, null, function () {
+                expect(params.res.code).to.be.equal('Access-Reject');
+                done();
+            });
+        });
+    });
+});
